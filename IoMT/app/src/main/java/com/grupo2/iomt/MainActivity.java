328578270aa5cd3a6b43aedb78c625162da6f1d7
Richard <rrruales@espol.edu.ec>
2019-06-12 21:16:12 -0500

    Función IniciarSesion() creada
    Función que valida las credenciales del usuario.

--- IoMT/app/src/main/java/com/grupo2/iomt/MainActivity.java
+++ IoMT/app/src/main/java/com/grupo2/iomt/MainActivity.java
@@ -2,13 +2,33 @@ package com.grupo2.iomt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUser, txtPasswd;
+    private Button btnLogin, btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        * Variables creadas referenciadas a los controles
+        * Richard*/
+        txtUser = (EditText) findViewById(R.id.txtUser);
+        txtPasswd = (EditText) findViewById(R.id.txtPasswd);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+    }
+
+    public void IniciarSesion(View view){
+        Intent intent =new Intent(this,Menu.class);
+        startActivity(intent);
     }
 }
