132cfef890e9001f2c5f3c496ad0aa7439fa8a72
Allan <allan_ov1995@hotmail.com>
2019-06-12 22:20:07 -0500

    Creación de Room para la base de datos y tabla Token
    Tanto la Room como la entidad y su DAO estan creadas

--- IoMT/app/src/main/java/com/grupo2/iomt/MainActivity.java
+++ IoMT/app/src/main/java/com/grupo2/iomt/MainActivity.java
@@ -1,8 +1,11 @@
 package com.grupo2.iomt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+
+import com.grupo2.iomt.db.DB;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DB db = instanceDB("mainDB");
+    }
+    private DB instanceDB(String name){
+        DB db = Room.databaseBuilder(this, DB.class, name)
+                .allowMainThreadQueries()
+                .build();
+        return db;
     }
 }
