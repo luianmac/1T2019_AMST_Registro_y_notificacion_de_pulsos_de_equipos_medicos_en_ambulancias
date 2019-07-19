4f9a12388897e6f96141fd1a1c2b704aac44aa27
Allan <allan_ov1995@hotmail.com>
2019-07-18 19:14:55 -0500

    comentarios extras

--- IoMT/app/src/main/java/com/grupo2/iomt/CheckInternet.java
+++ IoMT/app/src/main/java/com/grupo2/iomt/CheckInternet.java
@@ -13,18 +13,19 @@ public class CheckInternet {
     Autor: Allan Orellana
     Parametros: ninguno
     Descripcion: Realiza un ping (ICMP) al servidor DNS de Google. Basta con tener una respuesta para saber si hay conexion con Internet
+    Devuelve: True si hay error en la conexion, y false si no lo hay.
      */
     static boolean errorConexion() {
         Runtime runtime = Runtime.getRuntime();
         try {
             Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
             int     exitValue = ipProcess.waitFor();
-            return !(exitValue == 0);  //simplemente se cambia la logica a negativo
+            return !(exitValue == 0);  //True si no hay respuesta y False si sí la hay
         }
         catch (IOException e)          { e.printStackTrace(); }
         catch (InterruptedException e) { e.printStackTrace(); }
 
-        return true; // se cambia la logica
+        return true; // en caso de exister un error en la conexion
     }
 
 
